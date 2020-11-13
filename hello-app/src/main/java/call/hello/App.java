5f8bd2c97c4582180986d2f9d4f0e409f3a98926
E2440 <67650880+E2440@users.noreply.github.com>
2020-11-13 07:17:12 +0100

    Update App.java

--- hello-app/src/main/java/call/hello/App.java
+++ hello-app/src/main/java/call/hello/App.java
@@ -3,7 +3,7 @@ package call.hello;
 public class App
 {
 
-    private final String message = "Hello World from Java Maven Project! mehmet";
+    private final String message = "Hello World from Java Maven Project!";
 
     public App() {}
 
